d23a15e84bd209001376ec5d64b8c4d3f206c4d4
aborboll <aborboll@student.42.fr>
2021-10-31 18:13:59 +0100

    .

---
+++ ex00/Zombie.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/31 18:11:15 by aborboll          #+#    #+#             */
+/*   Updated: 2021/10/31 18:12:09 by aborboll         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/*
+** Incluimos las librerias esenciales.
+*/
+#include <iostream>
+#include <string.h>
+
+class Zombie
+{
+	private:
+		std::string	name;
+	public:
+};
