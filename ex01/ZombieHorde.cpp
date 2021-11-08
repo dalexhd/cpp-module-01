1a9b794f182b39b3c480119d374bdc85511748f7
DalexHD <eldalexhd@gmail.com>
2021-11-08 10:47:08 +0000

    feat: add zombie horde.

--- ex01/ZombieHorde.cpp
+++ ex01/ZombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/03 19:15:57 by aborboll          #+#    #+#             */
-/*   Updated: 2021/11/03 19:21:13 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/11/08 10:46:29 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,12 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
+	Zombie *zombieHorde = new Zombie[N];
+
+	for (int i = 0; i < N; i++)
+	{
+		zombieHorde[i].setName(name + "_" + std::to_string(i));
+		zombieHorde[i].announce();
+	}
+	return (&zombieHorde[0]);
 }
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: aborboll <aborboll@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/02 16:39:07 by aborboll          #+#    #+#             */
-/*   Updated: 2021/11/03 19:39:34 by aborboll         ###   ########.fr       */
+/*   Updated: 2021/11/08 10:45:36 by aborboll         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 int main(void)
 {
-	//TODO: This is not done yet!
+	Zombie *zombie;
+
+	zombie = zombieHorde(6, "zombie");
+	if (!zombie)
+		return (-1);
+	delete[] zombie;
+	return (0);
 }
